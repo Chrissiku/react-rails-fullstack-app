eb7113dd769ba1268e7fd3508d592b592ce03cc0
Chris Siku <chrissiku5@gmail.com>
2023-02-27 19:04:56 +0200

    Add Id

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -3,28 +3,42 @@ import * as ReactDom from "react-dom";
 
 const QuestionList = () => {
   const questions = [
-    { title: "How to check if a key is present in a Hash?", tag: "Ruby" },
     {
+      id: 1,
+      title: "How to check if a key is present in a Hash?",
+      tag: "Ruby",
+    },
+    {
+      id: 2,
       title: "What is the difference between strings and symbols?",
       tag: "Ruby",
     },
-    { title: "What happened if you add two same key Hash?", tag: "Ruby" },
-    { title: "How to delete a given key from a Hash?", tag: "Ruby" },
-    { title: "How to check if two Hashes are identical?", tag: "Ruby" },
-    { title: "How to combine two Hashes in Ruby?", tag: "Ruby" },
-    { title: "How to get unique keys from two Hashes in Ruby?", tag: "Ruby" },
     {
+      id: 3,
+      title: "What happened if you add two same key Hash?",
+      tag: "Ruby",
+    },
+    { id: 4, title: "How to delete a given key from a Hash?", tag: "Ruby" },
+    { id: 5, title: "How to check if two Hashes are identical?", tag: "Ruby" },
+    { id: 6, title: "How to combine two Hashes in Ruby?", tag: "Ruby" },
+    {
+      id: 7,
+      title: "How to get unique keys from two Hashes in Ruby?",
+      tag: "Ruby",
+    },
+    {
+      id: 8,
       title: "What does the Hash? key member ? and include method in a Hash",
       tag: "Ruby",
     },
-    { title: "What are blocks in Ruby?", tag: "Ruby" },
-    { title: "Does the order of hashes matter in Ruby?", tag: "Ruby" },
+    { id: 9, title: "What are blocks in Ruby?", tag: "Ruby" },
+    { id: 10, title: "Does the order of hashes matter in Ruby?", tag: "Ruby" },
   ];
   return (
     <div className="row">
       <div className="col-lg-10 mx-auto">
-        {questions.map((question, index) => (
-          <div className="card rounded-0 mt-3" key={index}>
+        {questions.map((question) => (
+          <div className="card rounded-0 mt-3" key={question.id}>
             <div className="card-body">
               <h3 className="card-title">{question.title}</h3>
               <p className="lead">
