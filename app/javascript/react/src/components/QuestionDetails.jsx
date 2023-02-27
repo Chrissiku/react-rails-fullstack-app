e48ad4b333970e4d264774f72c8c9efcf9fbd022
Chris Siku <chrissiku5@gmail.com>
2023-02-27 19:39:07 +0200

    Use states in our components

--- app/javascript/react/src/components/QuestionDetails.jsx
+++ app/javascript/react/src/components/QuestionDetails.jsx
@@ -1,7 +1,8 @@
-import * as React from "react";
-import * as ReactDom from "react-dom";
+import React, { useState } from "react";
 
 const QuestionDetails = (props) => {
+  const [likeCount, setLikeCount] = useState(0);
+
   return (
     <div className="card rounded-0 mt-3">
       <div className="card-body">
@@ -9,6 +10,25 @@ const QuestionDetails = (props) => {
         <p className="lead">
           <span className="badge bg-primary">{props.question.tag}</span>
         </p>
+        <button
+          type="button"
+          className="btn btn-primary mt-1"
+          onClick={() => setLikeCount(likeCount + 1)}
+        >
+          Like
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary mt-1"
+          onClick={() => setLikeCount(likeCount - 1)}
+        >
+          DisLike
+        </button>
+        {likeCount > 0 ? (
+          <span className="ms-2 badge bg-primary">{likeCount}</span>
+        ) : (
+          <span className="ms-2 badge bg-primary">{likeCount}</span>
+        )}
       </div>
     </div>
   );
